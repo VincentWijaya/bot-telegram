bdcc998c6d4e2cf92df553d3977c0163bbf71fa3
Vincent Wijaya <vincent@myboost.id>
2019-06-28 15:31:11 +0700

    finish action get user report

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Telegraf = require('telegraf');
 const { Markup } = require('telegraf');
+const axios = require('axios');
 require('dotenv').config();
 
 const helper = require('./helper');
@@ -33,8 +34,32 @@ bot.action('myReport', async (ctx) => {
   userId = ctx.from.id;
 
   try {
-    const checkUser = await helper.checkUser(`${server}/users/one/12345}`);
-    console.log(checkUser);
+    const checkUser = await helper.checkUser(`${server}/users/one/${userId}`);
+
+    if (checkUser == true) {
+      const { data: sellingData } = await axios.get(`${server}/selling/telegram/${userId}`);
+
+      if (sellingData.length === 0) {
+        ctx.editMessageText('You dont have any report!');
+      } else {
+        sellingData.forEach((item, index) => {
+          let total = 0
+          let product = `List item sold ${item.createdAt.toString().slice(0, 10)}`
+          item.selling.forEach(element => {
+            total += Number(element.Total)
+            product += `\n${element.itemName} = ${element.quantity} pcs = Rp.${element.Total.toLocaleString()}`
+          });
+
+          if (index === sellingData.length - 1) {
+            ctx.editMessageText(`${product} \nTotal : Rp.${total.toLocaleString()}`)
+          } else {
+            ctx.reply(`${product} \nTotal : Rp.${total.toLocaleString()}`)
+          }
+        });
+      }
+    } else {
+      ctx.editMessageText('😢 You are not registered! Please contact admin!');
+    }
   } catch (error) {
     console.log(error);
   }
